712feb0cd6ca7902251e02c6350ab96f166cd116
Nattan Sousa <nattan_lion@msn.com>
2022-07-13 17:22:33 -0300

    feat: Criando validações do update

--- app/Http/Requests/CakeUpdateRequest.php
+++ app/Http/Requests/CakeUpdateRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class CakeUpdateRequest extends FormRequest
 {
@@ -13,7 +16,24 @@ class CakeUpdateRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
+    }
+
+    public function validator($factory)
+    {
+        return $factory->make(
+            $this->sanitize(), $this->container->call([$this, 'rules']), $this->messages()
+        );
+    }
+
+    public function sanitize()
+    {
+        $this->merge([
+            'nome' => $this->json('nome'),
+            'peso' => $this->json('peso'),
+            'qtd_disponivel' => $this->json('qtd_disponivel')
+        ]);
+        return $this->all();
     }
 
     /**
@@ -24,7 +44,28 @@ class CakeUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'nome' => 'nullable',
+            'peso' => 'nullable|integer',
+            'qtd_disponivel' => 'nullable|min:1|integer',
         ];
+    }
+
+    public function messages()
+    {
+        return [
+            'peso.integer' => 'Peso deve ser um número inteiro.',
+            'qtd_disponivel.min' => 'A quantidade disponivel deve ser maior que :min.',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'status' => 'nook',
+                'msg' => 'Verifique os campos.',
+                'errors' => $validator->getMessageBag()->all()
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
     }
 }
