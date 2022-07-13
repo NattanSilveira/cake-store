41ef5c6eb42e5ba20ac51959882c830f4ba353e5
Nattan Sousa <nattan_lion@msn.com>
2022-07-13 16:49:31 -0300

    feat: Criado o template do email

---
+++ resources/views/mail/sendMail.blade.php
@@ -0,0 +1,4 @@
+Olá, {{ $mail->email }},</br></br>
+Há disponibilidade do bolo {{ $cake->nome }}</br></br>
+Realize sua reserva clicando abaixo.!</br></br>
+<button type="button"><a href="">Realizar Reserva</a></button>
